6c9d05a5d94f92a1b277504347cf94d84c18b291
leea51 <leea51@wwu.edu>
2018-01-31 16:20:04 -0800

    added 'surveys' to INSTALLED_APPS

--- proj/proj/settings.py
+++ proj/proj/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap3',
     'bootstrap_themes',
-    'login.apps.LoginConfig'
+    'login.apps.LoginConfig',
+    'surveys',
 ]
 
 MIDDLEWARE = [
