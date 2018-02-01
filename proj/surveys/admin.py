8971519e3d077c899e8c7f3de43af26fa832a038
mcdougr3 <mcdougr3@wwu.edu>
2018-01-31 17:01:27 -0800

    remade migrations

--- proj/surveys/admin.py
+++ proj/surveys/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question, Survey, Response, User
+
+admin.site.register(Question)
+admin.site.register(Survey)
+admin.site.register(Response)
+admin.site.register(User)
--- proj/surveys/models.py
+++ proj/surveys/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import uuid
 
 
-class Users(models.Model):
+class User(models.Model):
         user_Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
         username = models.CharField(max_length=45)
         email = models.CharField(max_length=255)
@@ -12,27 +12,20 @@ class Users(models.Model):
         last_name = models.CharField(max_length=45)
         role = models.CharField(max_length=1)
 
-        def __init_ (self):
-                self.user_id = uuid.uuid4()
-                #self.username = name
-                #self.password = passwd
-        def getUsername(self):
-                return self.username
-
-class Surveys(models.Model):
+class Survey(models.Model):
 	survey_Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-	creator_Id = models.ForeignKey(Users, on_delete=models.PROTECT)
+	creator_Id = models.ForeignKey(User, on_delete=models.PROTECT)
 
-class Questions(models.Model):
+class Question(models.Model):
 	question_Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-	question_survey_Id = models.ForeignKey(Surveys, on_delete=models.PROTECT)
+	question_survey_Id = models.ForeignKey(Survey, on_delete=models.PROTECT)
 	question_type = models.CharField(max_length=50)
 	question_text = models.CharField(max_length=400)
 
-class Responses(models.Model):
+class Response(models.Model):
 	response_Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-	response_question_Id = models.ForeignKey(Questions, on_delete=models.PROTECT)
+	response_question_Id = models.ForeignKey(Question, on_delete=models.PROTECT)
 	#response_question_type = models.ForeignKey(Questions, on_delete=models.PROTECT)
-	response_survey_Id 	= models.ForeignKey(Surveys, on_delete=models.PROTECT)
-	response_user_Id = models.ForeignKey(Users, on_delete=models.PROTECT)
+	response_survey_Id 	= models.ForeignKey(Survey, on_delete=models.PROTECT)
+	response_user_Id = models.ForeignKey(User, on_delete=models.PROTECT)
 	response_text = models.CharField(max_length=400)
