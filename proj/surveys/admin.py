8045bd79f1b1830e4cf3dc761cc68e02dddf77c0
leea51 <leealexanderg@gmail.com>
2018-02-11 23:23:20 -0800

    added Profile class some str methods for displaying class data

--- proj/surveys/admin.py
+++ proj/surveys/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question, Survey, Response
+from .models import Question, Survey, Response, Profile
 
 admin.site.register(Question)
 admin.site.register(Survey)
 admin.site.register(Response)
+admin.site.register(Profile)
--- proj/surveys/models.py
+++ proj/surveys/models.py
@@ -1,42 +1,68 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django import forms
 import uuid
+
 
 import json
 from uuid import UUID
 from json import JSONEncoder
 
 class UUIDEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, UUID):
-            # if the obj is uuid, we simply return the value of uuid
-            return obj.hex
-        return json.JSONEncoder.default(self, obj)
+        def default(self, obj):
+                if isinstance(obj, UUID):
+                        # if the obj is uuid, we simply return the value of uuid
+                        return obj.hex
+                return json.JSONEncoder.default(self, obj)
 
 '''
 class User(models.Model):
-		user_Id = models.UUIDField(primary_key=True, default=uuid.UUID(int=uuid.uuid4().int))
-		username = models.CharField(max_length=45)
-		#username = models.CharField(primary_key=True, max_length=45)
-		email = models.CharField(max_length=255)
-		password = models.CharField(max_length=32)
-		create_time = models.DateTimeField(auto_now=True)
-		first_name = models.CharField(max_length=45)
-		last_name = models.CharField(max_length=45)
-		role = models.CharField(max_length=1)
+        user_Id = models.UUIDField(primary_key=True, default=uuid.UUID(int=uuid.uuid4().int))
+        username = models.CharField(max_length=45)
+        #username = models.CharField(primary_key=True, max_length=45)
+        email = models.CharField(max_length=255)
+        password = models.CharField(max_length=32)
+        create_time = models.DateTimeField(auto_now=True)
+        first_name = models.CharField(max_length=45)
+        last_name = models.CharField(max_length=45)
+        role = models.CharField(max_length=1)
 '''
+
+class Profile(models.Model):
+        user = models.OneToOneField(User, on_delete=models.CASCADE)
+        role = models.CharField(max_length=1)
+        
+        def __str__(self):
+                u = self.user
+                return 'Username: %s, Name: %s %s' % (u.username, u.first_name, u.last_name)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+                Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 class Survey(models.Model):
 	survey_Id = models.UUIDField(primary_key=True, default=uuid.UUID(int=uuid.uuid4().int))
 	creator_Id = models.ForeignKey(User, on_delete=models.CASCADE)
 	title = models.CharField(max_length=100, null=True)
 
+	def __str__(self):
+                return 'Survey ID: %s, Title: %s, %s' % (self.survey_Id, self.title, self.creator_Id.profile)
+
 class Question(models.Model):
 	question_Id = models.UUIDField(primary_key=True, default=uuid.UUID(int=uuid.uuid4().int))
 	question_survey_Id = models.ForeignKey(Survey, on_delete=models.CASCADE)
 	question_type = models.CharField(max_length=20)
 	#question_text = models.CharField(max_length=400)
+
+	def __str__(self):
+                return 'Question ID: %s, %s' % (self.question_Id, self.question_survey_Id)
 
 class MCQuestion(models.Model):
 	question_Id = models.UUIDField(primary_key=True, default=uuid.uuid4)
