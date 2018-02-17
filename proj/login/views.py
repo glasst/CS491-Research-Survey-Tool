d67f2f86f96826bb2b1f32f350295b7659aaf355
leea51 <leealexanderg@gmail.com>
2018-02-16 17:19:21 -0800

    "removed unused import for user in login.views.py"

--- proj/login/views.py
+++ proj/login/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
 
 # experimental
 
