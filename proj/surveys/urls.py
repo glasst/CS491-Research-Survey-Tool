0520e6823c94bf8a1b9fc6fef17186324a2b4680
leea51 <leea51@wwu.edu>
2018-02-26 18:17:41 -0800

    rebasing

--- proj/surveys/urls.py
+++ proj/surveys/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
 app_name = 'surveys'
 
 urlpatterns = [
@@ -9,50 +8,28 @@ urlpatterns = [
     path('', views.home, name='home'),
 
     # ex: /surveys/newquestion/
-    # path('newquestion/', views.newquestion, name='newquestion'),
-=======
-#app_name = 'surveys'
-
-urlpatterns = [
-    # ex: /surveys/
-    path('', views.index, name='index'),
-
-    # ex: /surveys/newquestion/
     path('newquestion/', views.newquestion, name='newquestion'),
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
+
     path('takesurvey/', views.takesurvey, name='takesurvey'),
     path('survey-completion/', views.surveycompletion, name='survey-completion'),
 
     path('edit/', views.editsurvey, name='editsurvey'),
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
 
     # ex: /surveys/multiplechoice.html
     path('multiplechoice.html', views.multiplechoice, name='multiplechoice'),
     path('textentry.html', views.textentry, name='textentry'),
     path('checkbox.html', views.checkbox, name='checkbox'),
 
-    # /surveys/index
-    path('index', views.index, name='index'),
 
     path('index/add_survey/', views.add_survey, name='add_survey'),
 
     # Ex: /surveys/e06f103b-d6e3-4e77-9442-ef938b621276/
-    path('<uuid:survey_Id>/', views.detail, name='detail'),
 
     path('<uuid:survey_Id>/delete_question/', views.delete_question, name='delete_question'),
     path('<uuid:survey_Id>/add_question/', views.add_question, name='add_question'),
     # path('<uuid:survey_Id>/new_question/', views.new_question, name='new_question'),
-=======
 
-    # ex: /surveys/multiplechoice.html
-    path('multiplechoice.html', views.multiplechoice, name='multiplechoice'),
-    path('textentry.html', views.textentry, name='textentry'),
-    path('checkbox.html', views.checkbox, name='checkbox'),
 
-    #/surveys/index
-    path('index', views.survey_index, name='survey_index'),
-
-    #/surveys/e06f103b-d6e3-4e77-9442-ef938b621276/
     path('<uuid:survey_Id>/', views.survey_detail, name='survey')
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
-]
+
+    path('<uuid:survey_Id>/deleteq/', views.deleteq, name='deleteq')
--- proj/surveys/views.py
+++ proj/surveys/views.py
@@ -1,10 +1,5 @@
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponseRedirect
-=======
-from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
 from django.contrib.auth.models import User
 from .models import Survey, Question, MCQuestion, TEQuestion, CBQuestion, Response, SurveyForm, QuestionForm, \
     MCQuestionForm, TEQuestionForm, CBQuestionForm, TakeSurveyForm
@@ -25,7 +20,6 @@ class UUIDEncoder(json.JSONEncoder):
 
 # Create your views here.
 
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
 
 def home(request):
     num_users = User.objects.all().count()
@@ -54,45 +48,13 @@ def home(request):
             #return render(request, 'newquestion.html', {'surveyID': survey.survey_Id})
             #return render(request, 'newquestion.html', {'newquestion': })
             return HttpResponseRedirect('edit/?id=' + str(n.pk))
-=======
-def index(request):
-    num_users = User.objects.all().count()
-    num_surveys = Survey.objects.all().count()
-    creator = User.objects.get(username=request.user.username)
-    survey_list = Survey.objects.filter(creator_Id=creator)
-
-    if request.method == 'POST':
-        #form = SurveyForm(request.POST or None, initial={'creator_Id':creator,})
-        form = SurveyForm(request.POST)
-        #form.fields['creator_Id'] = creator
-
-        #usr = form.set_creator_foreign_key(creator)
-
-        if form.is_valid():
-            #creator = request.user
-            #form.set_creator_foreign_key(creator)
-
-            #form.cleaned_data['creator_Id'] = creator
-            form.save()
-
-            #bbb = form.save(commit=False)
-            #bbb.creator_Id = creator
-            #bbb.save()
-
-            return HttpResponseRedirect('newquestion/')
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
     else:
         form = SurveyForm()
 
     return render(
         request,
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
         'home.html',
         context={'form': form, 'num_users': num_users, 'num_surveys': num_surveys, 'userID': creator},
-=======
-        'index.html',
-        context={'form':form, 'num_users':num_users, 'num_surveys':num_surveys, 'userID':creator},
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
     )
 
 
@@ -105,32 +67,19 @@ def editsurvey(request):
         try:
             sid = UUID(request.session['survey'], version=4)
             s = Survey.objects.get(survey_Id=sid)
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
         except:
             return HttpResponseRedirect('/surveys')
-=======
-        except: return HttpResponseRedirect('/surveys')
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
 
     if not s: return HttpResponseRedirect('/surveys')
 
     if request.method == 'POST':
         if 'add' in request.POST and request.POST['add']:
             typ = request.POST['type']
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
-            try:
-                s = Survey.objects.get(survey_Id=request.session['survey'])
-            except:
-                return HttpResponseRedirect('/surveys')
-            if typ == 'MC':
-                q = MCQuestion(question_survey_Id=s, question_text=request.POST['add'], option_1=request.POST['op1'],
-                               option_2=request.POST['op2'])
-=======
+
             try: s = Survey.objects.get(survey_Id=request.session['survey'])
             except: return HttpResponseRedirect('/surveys')
             if typ == 'MC':
                 q = MCQuestion(question_survey_Id=s, question_text=request.POST['add'], option_1=request.POST['op1'], option_2=request.POST['op2'])
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
                 q.save()
         elif 'remove' in request.POST:
             MCQuestion.objects.get(question_Id=request.POST['remove']).delete()
@@ -138,15 +87,6 @@ def editsurvey(request):
     return render(
         request,
         'edit.html',
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
-        context={'survey': request.session['survey'],
-                 'mcquestions': MCQuestion.objects.filter(question_survey_Id=s.survey_Id)},
-    )
-
-
-
-
-=======
         context={'survey':request.session['survey'], 'mcquestions':MCQuestion.objects.filter(question_survey_Id=s.survey_Id)},
     )
 
@@ -195,7 +135,6 @@ def multiplechoice(request):
         'multiplechoice.html',
         context={'form':form},
     )
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
 
 def multiplechoice(request):
     if request.method == 'POST':
@@ -206,133 +145,87 @@ def multiplechoice(request):
     else:
         form = MCQuestionForm()
 
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
-    return render(
-        request,
-        'multiplechoice.html',
-        context={'form': form},
-    )
-
-=======
-def textentry(request):
-    if request.method == 'POST':
-        form = TEQuestionForm(request.POST)
-        if form.is_valid():
-
-            form.save() #save to DB
-            return HttpResponseRedirect('/surveys/newquestion')
-    else:
-        form = TEQuestionForm()
-
-    return render(
-        request,
-        'textentry.html',
-        context={'form':form},
-    )
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
 
 def textentry(request):
-    if request.method == 'POST':
-        form = TEQuestionForm(request.POST)
-        if form.is_valid():
-            form.save()  # save to DB
-            return HttpResponseRedirect('/surveys/newquestion')
-    else:
-        form = TEQuestionForm()
+	if request.method == 'POST':
+		form = TEQuestionForm(request.POST)
+		if form.is_valid():
 
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
-    return render(
-        request,
-        'textentry.html',
-        context={'form': form},
-    )
+			form.save() #save to DB
+			return HttpResponseRedirect('/surveys/newquestion')
+	else:
+		form = TEQuestionForm()
+
+	return render(
+		request,
+		'textentry.html',
+		context={'form':form},
+)
 
 
 def checkbox(request):
-    if request.method == 'POST':
-        form = CBQuestionForm(request.POST)
-        if form.is_valid():
-            form.save()  # save to DB
-            return HttpResponseRedirect('/surveys/newquestion')
-    else:
-        form = CBQuestionForm()
-=======
-def checkbox(request):
-    if request.method == 'POST':
-        form = CBQuestionForm(request.POST)
-        if form.is_valid():
+	if request.method == 'POST':
+		form = CBQuestionForm(request.POST)
+		if form.is_valid():
 
-            form.save() #save to DB
-            return HttpResponseRedirect('/surveys/newquestion')
-    else:
-        form = CBQuestionForm()
+			form.save() #save to DB
+			return HttpResponseRedirect('/surveys/newquestion')
+	else:
+		form = CBQuestionForm()
 
-    return render(
-        request,
-        'checkbox.html',
-        context={'form':form},
-    )
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
-
-    return render(
-        request,
-        'checkbox.html',
-        context={'form': form},
-    )
+	return render(
+		request,
+		'checkbox.html',
+		context={'form':form},
+)
 
 
 ### VIEWS FOR SURVEY TAKING ###
 def takesurvey(request):
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
-=======
 
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
-    if request.method == 'POST':
-        form = TakeSurveyForm(request.POST, user=request.user)
-        if form.is_valid():
-            request.session['survey_to_take'] = getattr(form.cleaned_data.get('survey_to_take'), 'survey_Id').hex
-            return HttpResponseRedirect('/surveys/survey-completion')
-    else:
-        form = TakeSurveyForm(user=request.user)
-    return render(
-        request,
-        'takesurvey.html',
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
-        {'form': form}
-=======
-        {'form':form}
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
-    )
+	if request.method == 'POST':
+		form = TakeSurveyForm(request.POST, user=request.user)
+		if form.is_valid():
+			request.session['survey_to_take'] = getattr(form.cleaned_data.get('survey_to_take'), 'survey_Id').hex
+			return HttpResponseRedirect('/surveys/survey-completion')
+	else:
+		form = TakeSurveyForm(user=request.user)
+	return render(
+		request,
+		'takesurvey.html',
+		{'form':form}
+)
 
 
 def surveycompletion(request):
-    surveyid = request.session.get('survey_to_take')
-    questions = Question.objects.filter(question_survey_Id=surveyid)
+	surveyid = request.session.get('survey_to_take')
+	questions = Question.objects.filter(question_survey_Id=surveyid)
 
-    mclist = []
-    telist = []
-    cblist = []
+	mclist = []
+	telist = []
+	cblist = []
 
-    # Still need to get cross-Question table querying
-    for q in questions:
-        if q.question_type == 'MC':
-            qq = MCQuestion.objects.filter(question_Id=q.question_Id)
-            mclist.append(qq)
+	# Still need to get cross-Question table querying
+	for q in questions:
+		qid = q.question_Id
 
-        if q.question_type == 'TE':
-            qq = MCQuestion.objects.filter(question_Id=q.question_Id)
-            telist.append(qq)
+		if q.question_type == 'MC':
+			qq = MCQuestion.objects.filter(question_Id=qid)
+			mclist.append(qq)
 
-        if q.question_type == 'CB':
-            qq = MCQuestion.objects.filter(question_Id=q.question_Id)
-            cblist.append(qq)
+		if q.question_type == 'TE':
+			qq = MCQuestion.objects.filter(question_Id=qid)
+			telist.append(qq)
 
-<<<<<<< cef8a8edfff97ea5b8a2b81c5082380756be005b
-    return render(
-        request,
-        'survey-completion.html',
-        {'surveyid': surveyid, 'mclist': mclist, 'telist': telist, 'cblist': cblist}
-    )
+		if q.question_type == 'CB':
+			qq = MCQuestion.objects.filter(question_Id=qid)
+			cblist.append(qq)
+
+	return render (
+		request,
+		'survey-completion.html',
+		{'surveyid':surveyid, 'allQ':questions, 'mclist':mclist, 'telist':telist, 'cblist':cblist}
+)
 
 
 # prints list of all survey objects
@@ -424,49 +317,3 @@ def delete_question(request, survey_Id):
     else:
         question.delete()
     return render(request, 'surveys/detail.html', {'survey': survey})
-
-'''
-def add_survey(request, survey_Id):
-    survey = get_object_or_404(Survey, survey_Id=survey_Id)
-    try:
-        selected_question = survey.question_set.get(question_Id=request.POST['question'])
-    except (KeyError, Question.DoesNotExist):
-        return render(request, 'surveys/detail.html', {
-            'question': question,
-            'error_message': "You did not select a valid question",
-        })
-    else:
-        selected_question.delete()
-
-    return render(request, 'surveys/detail.html', {'survey': survey})
-'''
-=======
-    return render (
-        request,
-        'survey-completion.html',
-        {'surveyid':surveyid, 'mclist':mclist, 'telist':telist, 'cblist':cblist}
-    )
-
-# prints list of all survey objects
-def survey_index(request):
-    all_surveys = Survey.objects.all()
-    return render(request, 'surveys/index.html', {'all_surveys': all_surveys})
-
-'''
-    html = ''
-
-    for survey in all_surveys:
-        url = str(survey.survey_Id) + '/'
-        html += '<a href="' + url + '">' + str(survey.survey_Id) + '</a><br>'
-    return HttpResponse(html)
-'''
-
-#prints survey id of specific survey
-def survey_detail(request, survey_Id):
-    #return HttpResponse("<h2>Survey ID: %s </h2>" % str(survey_Id))
-    #survey = Survey.objects.filter(survey_Id=survey_Id).get()
-
-    survey = get_object_or_404(Survey, survey_Id=survey_Id)
-        
-    return render(request, 'surveys/detail.html', {'survey': survey})
->>>>>>> added alt survey index redirecting to individual survey pages listing uuid of questions
