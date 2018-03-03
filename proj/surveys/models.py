0259abb1bbecf9514997887270bee44b779e8d3a
Trevor J. Glass <glasst@LabImage2016-17>
2018-03-03 13:35:57 -0800

    merging updated version

--- proj/surveys/models.py
+++ proj/surveys/models.py
@@ -20,45 +20,11 @@ class UUIDEncoder(json.JSONEncoder):
             return obj.hex
         return json.JSONEncoder.default(self, obj)
 
-
-'''
-class User(models.Model):
-        user_Id = models.UUIDField(primary_key=True, default=uuid.UUID(int=uuid.uuid4().int))
-        username = models.CharField(max_length=45)
-        #username = models.CharField(primary_key=True, max_length=45)
-        email = models.CharField(max_length=255)
-        password = models.CharField(max_length=32)
-        create_time = models.DateTimeField(auto_now=True)
-        first_name = models.CharField(max_length=45)
-        last_name = models.CharField(max_length=45)
-        role = models.CharField(max_length=1)
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=1)
-
-    def __str__(self):
-        u = self.user
-        return 'Username: %s, Name: %s %s' % (u.username, u.first_name, u.last_name)
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
-'''
-
-
 class Survey(models.Model):
     survey_Id = models.UUIDField(primary_key=True, default=uuid.UUID(int=uuid.uuid4().int))
     creator_Id = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100, null=True)
-    num_questions = models.IntegerField(default=0)
+    #num_questions = models.IntegerField(default=0)
 
     def __str__(self):
         return 'Survey ID: %s, Title: %s, %s' % (self.survey_Id, self.title, self.creator_Id)
@@ -84,11 +50,6 @@ class MCQuestion(models.Model):
     option_4 = models.CharField(max_length=100)
     option_5 = models.CharField(max_length=100)
 
-    # increment number of questions in survey and set current question number
-    def save(self):
-        question_survey_Id.num_questions += 1
-        question_num = question_survey_Id.num_questions
-        super(MCQuestion, self).save()
 
 
 class TEQuestion(models.Model):
@@ -97,11 +58,6 @@ class TEQuestion(models.Model):
     question_text = models.CharField(max_length=400)
     question_num = models.IntegerField()
 
-    # increment number of questions in survey and set current question number
-    def save(self):
-        question_survey_Id.num_questions += 1
-        question_num = question_survey_Id.num_questions
-        super(MCQuestion, self).save()
 
 class CBQuestion(models.Model):
     question_Id = models.UUIDField(primary_key=True, default=uuid.UUID(int=uuid.uuid4().int))
@@ -114,10 +70,6 @@ class CBQuestion(models.Model):
     option_4 = models.CharField(max_length=100)
     option_5 = models.CharField(max_length=100)
 
-def save(self):
-        question_survey_Id.num_questions += 1
-        question_num = question_survey_Id.num_questions
-        super(MCQuestion, self).save()
 
 
 class ResponseTE(models.Model):
@@ -129,17 +81,4 @@ class ResponseTE(models.Model):
     response_user_Id = models.ForeignKey(User, on_delete=models.PROTECT)
     response_text = models.CharField(max_length=400)
 
-'''
-OPTION_CHOICES = (
-    ('CB', 'CheckBox'),
-    ('MC', 'MultipleChoice'),
-)
 
-class Option(models.Model):
-    option_Id = models.UUIDField(primary_key=True, default=uuid.UUID(int=uuid.uuid4().int))
-    type_of_question = models.CharField(max_length=2, choices=OPTION_CHOICES)
-    mc_question_Id = models.ForeignKey(MCQuestion, on_delete=models.PROTECT)
-    cb_question_Id = models.ForeignKey(CBQuestion, on_delete=models.PROTECT)
-    #question_Id = models.GenericForeignKey()
-
->>>>>>> 8654c05800755b7a9f16ae29902fde702853adf6
